514e9330646d574cbcb229d4ff61dfae84428d7f
Christine Godfrey <cldonnen2001@yahoo.com>
2021-02-24 19:53:27 -0600

    initial commit SyntaxSugarExercise

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            //    if (answer < 9)
+            //    {
+            //        response = answer + " is less than nine";
+            //    }
+            //    else
+            //    {
+            //        response = answer + " greater than or equal to nine";
+            //    }
+            //    Console.WriteLine(response);
+            //}
+            var response = (answer < 9) ? " is less than nine" : " is greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+
+}
